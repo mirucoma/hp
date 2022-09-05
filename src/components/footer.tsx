a72ad6a7111dd59f38de1d88085583db5e238c4b
kra <koki@asai.email>
2022-09-05 12:15:06 +0900

    fix: #31 (#40)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,9 @@
-import React from "react"
 import { css } from "@emotion/react"
-import color from "../styles/color"
-import mq from "../styles/mediaQuery"
 import { Link } from "gatsby"
+import mq from "../styles/mediaQuery"
 
 export default () => {
-  const footerWrapper = css({
-    marginTop: "24px",
-    backgroundColor: "#F5F5F5",
-    fontSize: "1.7rem",
-    color: "#797979",
-  })
-
-  const field = css({
+  const menuStyle = css({
     paddingTop: "24px",
     display: "flex",
     justifyContent: "center",
@@ -20,6 +11,7 @@ export default () => {
 
     a: {
       color: "#797979",
+      fontSize: "1.2em",
     },
 
     "a:hover": {
@@ -34,35 +26,41 @@ export default () => {
     },
   })
 
-  const bottomText = css({
-    padding: "24px",
-    textAlign: "center",
-  })
   return (
-    <>
-      <div css={footerWrapper}>
-        <div css={field}>
-          <Link to="/user-policy">
-            <p>利用規約</p>
-          </Link>
-          <Link to="/privacy-policy">
-            <p>プライバシーポリシー</p>
-          </Link>
-          <Link to="/terms-of-sale">
-            <p>特定商取引法に基づく表記</p>
-          </Link>
-          <Link to="/company">
-            <p>運営会社</p>
-          </Link>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdTRSPZJ9PS5lAXBJTui5n_25DscUlz09jy333w1GcrFkLWvg/viewform">
-            <p>お問い合わせ</p>
-          </a>
-        </div>
-        <p css={bottomText}>
-          ©2022 Lagoon Corporation, inc. Icon made by Font Awesome is licensed
-          by CC 4.0BY
-        </p>
+    <div
+      css={{
+        marginTop: "24px",
+        paddingBottom: "128px",
+        backgroundColor: "#F5F5F5",
+        fontSize: "12px",
+        color: "#797979",
+      }}
+    >
+      <div css={menuStyle}>
+        <Link to="/user-policy">
+          <p>利用規約</p>
+        </Link>
+        <Link to="/privacy-policy">
+          <p>プライバシーポリシー</p>
+        </Link>
+        <Link to="/terms-of-sale">
+          <p>特定商取引法に基づく表記</p>
+        </Link>
+        <Link to="/company">
+          <p>運営会社</p>
+        </Link>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdTRSPZJ9PS5lAXBJTui5n_25DscUlz09jy333w1GcrFkLWvg/viewform">
+          <p>お問い合わせ</p>
+        </a>
       </div>
-    </>
+      <p
+        css={{
+          padding: "24px",
+          textAlign: "center",
+        }}
+      >
+        ©2022 Lagoon Corporation, inc.
+      </p>
+    </div>
   )
 }
